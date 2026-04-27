bf6f88df40a17aee5b9986dd50316f753f657a40
Marcos <kitoscastro294@gmail.com>
2026-04-27 13:46:03 -0600

    Creacion de componente de registro de refugios, se agrego el router-dom para mandarlo a una página a parte

--- RefugiosMascotas/src/main.tsx
+++ RefugiosMascotas/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // <-- Paso 1: Importar
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter> {/* <-- Paso 2: Envolver */}
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
